fc94cffb6cd09d6b508a77ff2a6694c64dcc261f
hkumarrai1 <166382429+hkumarrai1@users.noreply.github.com>
2024-06-27 15:34:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const signInForm = document.querySelector('.sign-in-form');
-const signUpForm = document.querySelector('.sign-up-form');
-const signUpLink = document.querySelector('#sign-up-link');
 
-signUpLink.addEventListener('click', () => {
-    signInForm.style.transform = 'translateX(-100%)';
-    signUpForm.style.transform = 'translateX(0)';
-});
-
-document.querySelector('#sign-up-btn').addEventListener('click', () => {
-    signUpForm.style.transform = 'translateX(100%)';
-    signInForm.style.transform = 'translateX(0)';
-});
